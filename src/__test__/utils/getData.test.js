cada05792321cee9c4941deb4bbca2582c3cc04a
Estiben <elysestiben@gmail.com>
2021-02-22 11:35:37 -0400

    prueba

--- src/__test__/utils/getData.test.js
+++ src/__test__/utils/getData.test.js
@@ -7,7 +7,6 @@ describe('Fetch api', () => {
 
     test('Llamar a una API y retornar datos', () => {
         fetch.mockResponseOnce(JSON.stringify({ data: '12345' }));
-
          return getData('https://google.com')//esta url no tiene nada que ver aqui, solo la enviamos como parametro porque lo requiere. sino fuese un mock si deberia ir la ruta real del api
         .then(res => {
             expect(res.data).toEqual('12345');
